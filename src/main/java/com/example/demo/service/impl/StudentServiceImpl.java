be7f1c967bea36f9c1eb67a363f0a4c85ddef675
bala-amypo <bala@amypo.in>
2025-12-18 04:53:13 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/StudentServiceImpl.java
@@ -2,9 +2,10 @@ package com.example.demo.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.example.demo.service.StudentService;
-import com.example.demo.repository.StudentRepository;
+
 import com.example.demo.entity.Student;
+import com.example.demo.repository.StudentRepository;
+import com.example.demo.service.StudentService;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,7 +16,6 @@ public class StudentServiceImpl implements StudentService {
     @Autowired
     private StudentRepository stdrepo;
 
-    
     @Override
     public Student poststudent(Student student) {
         return stdrepo.save(student);
@@ -26,29 +26,8 @@ public class StudentServiceImpl implements StudentService {
         return stdrepo.findAll();
     }
 
-    
     @Override
-    public Student getStudentById(Long id) {
-        Optional<Student> student = stdrepo.findById(id);
-        return student.orElse(null); 
-     }
-     
-//     @Override
-//     public Student updateStudent(Long id, Student studentDetails) {
-//         Optional<Student> optionalStudent = stdrepo.findById(id);
-//         if (optionalStudent.isPresent()) {
-//             Student existingStudent = optionalStudent.get();
-//             existingStudent.setName(studentDetails.getName());
-//             existingStudent.setDept(studentDetails.getDept());
-//             existingStudent.setDob(studentDetails.getDob());
-//             existingStudent.setCgpa(studentDetails.getCgpa());
-//             return stdrepo.save(existingStudent);
-//         } else {
-//             return null; 
-//         }
-//     }
-//     @Override
-//     public void deleteStudent(Long id) {
-//         stdrepo.deleteById(id);
-//     }
+    public Optional<Student> getStudentById(Long id) {
+        return stdrepo.findById(id);
+    }
 }
